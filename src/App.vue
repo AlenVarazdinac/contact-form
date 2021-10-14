548e46319b4acd6e6b9007e646dd0fd666a63d33
Alen Varazdinac <alen.varazdinac@gmail.com>
2021-10-14 14:34:52 +0200

    feat(Form): form layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form class="form" @submit.prevent="submitForm">
+      <div class="input-wrapper">
+        <label for="name">Name<span style="color: red"> *</span></label>
+        <input v-model="form.name" id="name" type="text" />
+      </div>
+      <div class="input-wrapper">
+        <label for="email">Email<span style="color: red"> *</span></label>
+        <input v-model="form.email" id="email" type="email" />
+      </div>
+      <div class="input-wrapper">
+        <label for="subject"
+          >Subject<span style="color: grey"> (optional)</span></label
+        >
+        <input v-model="form.subject" id="subject" type="text" />
+      </div>
+      <div class="input-wrapper">
+        <label for="message">Message<span style="color: red"> *</span></label>
+        <textarea v-model="form.message" id="message"></textarea>
+      </div>
+      <button type="submit">Send</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      },
+    }
   },
-};
+  methods: {
+    submitForm() {
+      console.log("form", this.form)
+    },
+  },
+}
 </script>
 
 <style lang="scss">
@@ -23,6 +53,30 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  & button {
+    display: flex;
+    align-self: flex-end;
+  }
+}
+
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
 }
 </style>
